63a190116d077d33aa65bd9be2e947175fe1bbf2
abdullah jamshed <abdullahjamshad01@gmail.com>
2021-06-10 16:05:12 +0500

    added ecosystem.config file & install pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+export default {
+  app: [
+    {
+      name: "server",
+      script: "./src/server.js",
+      env: {
+        NODE_ENV: process.env.NODE_ENV || "development",
+        PORT: process.env.PORT,
+        BUCKET_NAME: process.env.BUCKET_NAME,
+      },
+    },
+  ],
+};
